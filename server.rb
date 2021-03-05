1e44bd7a92b23a44bf919508c99e05c4c9aa85ee
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-03-04 22:04:31 -0500

    Created routes to add new inventions to list

---
+++ server.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+
+inventions = [
+    {:invention => "High-pressure piston steam engine", :inventor => "Jacob Leupold", :year => 1720},
+    {:invention => "Electrical defibrillator", :inventor => "Dr. Albert Hyman", :year => 1933},
+    {:invention => "Ball bearing", :inventor => "Philip Vaughan", :year => 1794},
+]
+
+get '/' do
+    "<h1>Welcome to the list of significant inventions</h1>"
+end
+
+get '/inventions' do
+    @inventions = inventions
+    erb :inventions
+end
+
+get '/newInvention' do
+    erb :new_invention
+end
+
+post '/addNewInvention' do
+    "<h2>#{params}</h2>"
+    inventions.push(params)
+    redirect to ('/inventions')
+end
